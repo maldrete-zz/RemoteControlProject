94ca8e42091cc6a8c37f72bfe629310d2f52bd64
Matt <mra286@humboldt.edu>
2019-11-08 17:32:23 -0700

    added powerON, greetMessage method on do while loop

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
 
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
-
+		Scanner kb = new Scanner(System.in);
+		
+		String input = "";
+		
+		do {
+			greetMessage();
+			input = kb.next();
+			
+			} while(! input.contentEquals("ON"));
+		
+		powerOn();
+			
+		}
+	
+	
+	public static void greetMessage() {
+		System.out.println("To begin, turn on the remote control:");
 	}
+	public static void powerOn() {
+		System.out.println("TURNING ON...");
+	}
+	
 	
 	// TODO: Add pressButton() method
 	// TODO: Add turnOn() method
 
 }
+//If the user inputs anything other than ON, the initial 
+// message repeats until ON is entered.
+
